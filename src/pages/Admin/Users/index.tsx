2ff074e727f984df89f0a3b7e567d2fe81e8f362
Albert <>
2020-11-02 18:20:41 -0500

    Users working

--- src/pages/Admin/Users/index.tsx
+++ src/pages/Admin/Users/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from 'antd/lib/typography/Title';
 
 import { useUsersQuery } from 'graphql/generated';
 import UsersTable from 'ui/Users/Table';
 
 const Users: React.FC = () => {
-  const [{ data, fetching }, getUsers] = useUsersQuery({ variables: { limit: 10 } });
+  const [limit, setLimit] = useState<number>(10);
+  const [page, setPage] = useState<number>(1);
+  const [search, setSearch] = useState<string>();
+  const [order, setOrder] = useState<string>();
+  const [orderBy, setOrderBy] = useState<string>();
+  const [{ data, fetching }] = useUsersQuery(
+    { variables: { limit, page, search, order, orderBy } },
+  );
 
   if (fetching && !data) {
     return <div> Loading... </div>;
@@ -21,10 +28,14 @@ const Users: React.FC = () => {
           loading={fetching}
           users={data.users.data}
           pagination={data.users.pagination}
-          // fetchMore={console.log}
-          fetchMore={(variables) => {
-            console.log(variables);
-            getUsers({ ...variables, requestPolicy: 'network-only' });
+          fetchMore={({ page: page_, limit: limit_, search: search_, order: order_ }) => {
+            setLimit(limit_);
+            setPage(page_);
+            setSearch(search_);
+            if (order_) {
+              setOrder(order_.order);
+              setOrderBy(order_.orderBy);
+            }
           }}
         />
       </div>
