ebc9b44ea3d32e72b73d35b5f2c7f38e6989dac4
Albert <>
2020-11-17 08:07:23 -0500

    organize code

--- src/start/StartApp.tsx
+++ src/start/StartApp.tsx
@@ -1,22 +1,11 @@
 import React, { useRef } from 'react';
 import { IntlProvider } from 'react-intl';
-import {
-  createClient,
-  Provider as URQLProvider,
-  dedupExchange,
-  fetchExchange,
-  // cacheExchange,
-} from 'urql';
-import { cacheExchange } from '@urql/exchange-graphcache';
-import { retryExchange } from '@urql/exchange-retry';
+import { Provider as URQLProvider } from 'urql';
 
 import { GlobalContext, useGlobal } from 'use/global';
 import Routes from './Routes';
-import constants from 'config/constants';
 import UserContext, { useUser } from 'use/user/UserContext';
-import { StorageItems } from 'util/Storage';
-import { removeByColumn } from 'util/stateHandler/items';
-import { TranslationsDocument } from 'graphql/generated';
+import getURQLClient from './getURQLClient';
 
 const GlobalStatus: React.FC = () => {
   const global = useGlobal();
@@ -33,68 +22,16 @@ const GlobalStatus: React.FC = () => {
   );
 };
 
-const cache = cacheExchange({
-  keys: {
-    Pagination: () => null,
-    TranslationPaginationResponse: () => 'TranslationPaginationResponse',
-    UserPaginationResponse: () => 'UserPaginationResponse',
-  },
-  updates: {
-    Mutation: {
-      translationDelete: (result, { id }, cache, info) => {
-        console.log(cache.readQuery({ query: TranslationsDocument }));
-        cache.updateQuery({ query: TranslationsDocument }, (data) => {
-          console.log(data, id);
-          // removeByColumn(data, 'translationID', translationID);
-          return data;
-        });
-      },
-      translationCreate: (result, { id }, cache, info) => {
-        console.log(cache.readQuery({ query: TranslationsDocument }));
-        cache.updateQuery({ query: TranslationsDocument }, (data) => {
-          console.log(data, id);
-          // removeByColumn(data, 'translationID', translationID);
-          return data;
-        });
-      }
-    },
-  },
-});
-
 const StartApp: React.FC = () => {
   const { setUser, ...props } = useUser();
-  const client = useRef(createClient({
-    url: `${constants.urlServer}/graphql/public`,
-    fetchOptions: () => {
-      const token = localStorage.getItem('accessToken');
-      return {
-        headers: { authorization: token ? `Bearer ${token}` : '' },
-        withCredentials: true,
-      };
-    },
-    exchanges: [
-      cache,
-      dedupExchange,
-      retryExchange({
-        retryIf: (error) => {
-          if (error && error.response && error.response.status === 401) {
-            setUser();
-            localStorage.removeItem(StorageItems.accessToken);
-            localStorage.removeItem(StorageItems.langID);
-            localStorage.removeItem(StorageItems.userID);
-          }
-          return false;
-        },
-      }),
-      fetchExchange,
-    ],
-  })).current;
+  const urql = useRef(getURQLClient(setUser)).current;
   return (
     <UserContext.Provider value={{ ...props, setUser }}>
-      <URQLProvider value={client}>
+      <URQLProvider value={urql}>
         <GlobalStatus />
       </URQLProvider>
     </UserContext.Provider>
   );
 };
+
 export default StartApp;
---
+++ src/start/getURQLClient.ts
@@ -0,0 +1,71 @@
+import {
+  createClient,
+  dedupExchange,
+  fetchExchange,
+  // cacheExchange,
+} from 'urql';
+import { retryExchange } from '@urql/exchange-retry';
+
+import constants from 'config/constants';
+import { StorageItems } from 'util/Storage';
+import { removeByColumn } from 'util/stateHandler/items';
+import { TranslationsDocument } from 'graphql/generated';
+
+/*
+const cache = cacheExchange({
+  keys: {
+    Pagination: () => null,
+    TranslationPaginationResponse: () => 'TranslationPaginationResponse',
+    UserPaginationResponse: () => 'UserPaginationResponse',
+  },
+  updates: {
+    Mutation: {
+      translationDelete: (result, { id }, cache, info) => {
+        console.log(cache.readQuery({ query: TranslationsDocument }));
+        cache.updateQuery({ query: TranslationsDocument }, (data) => {
+          console.log(data, id);
+          // removeByColumn(data, 'translationID', translationID);
+          return data;
+        });
+      },
+      translationCreate: (result, { id }, cache, info) => {
+        console.log(cache.readQuery({ query: TranslationsDocument }));
+        cache.updateQuery({ query: TranslationsDocument }, (data) => {
+          console.log(data, id);
+          // removeByColumn(data, 'translationID', translationID);
+          return data;
+        });
+      }
+    },
+  },
+});
+*/
+
+const client = (setUser: () => void) => createClient({
+  url: `${constants.urlServer}/graphql/public`,
+  fetchOptions: () => {
+    const token = localStorage.getItem('accessToken');
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+      withCredentials: true,
+    };
+  },
+  exchanges: [
+    // cache,
+    dedupExchange,
+    retryExchange({
+      retryIf: (error) => {
+        if (error && error.response && error.response.status === 401) {
+          setUser();
+          localStorage.removeItem(StorageItems.accessToken);
+          localStorage.removeItem(StorageItems.langID);
+          localStorage.removeItem(StorageItems.userID);
+        }
+        return false;
+      },
+    }),
+    fetchExchange,
+  ],
+});
+
+export default client;
