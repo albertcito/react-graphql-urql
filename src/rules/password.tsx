56c911cc6b4c7513590a79fa4a2af27f86724a12
albertcito <alberttjornehoj@gmail.com>
2020-12-18 07:54:03 -0500

    Added type to  password validator

--- src/rules/password.tsx
+++ src/rules/password.tsx
@@ -1,5 +1,10 @@
-const rulePass = (getFieldValue: any, field: string) => ({
-  validator(rule: any, value: any) {
+import { RuleObject } from 'antd/lib/form';
+
+const rulePass = (
+  getFieldValue: (name: string) => string,
+  field: string,
+) => ({
+  validator(_: RuleObject, value: string) {
     if (!value || getFieldValue(field) === value) {
       return Promise.resolve();
     }
